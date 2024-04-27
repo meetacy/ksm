0c855d6c0aedb8a1f7935da38cabe9fb30f8a1f7
y9san9 <y9san9@gmail.com>
2024-04-27 13:10:27 +0300

    fix: rename previous/root to previousController/rootController

--- navigation/src/commonMain/kotlin/ksm/navigation/stack/StateController.kt
+++ navigation/src/commonMain/kotlin/ksm/navigation/stack/StateController.kt
@@ -3,10 +3,10 @@ package ksm.navigation.stack
 import ksm.StateController
 import ksm.asStateController
 
-public val StateController.previous: StateController get() {
+public val StateController.previousController: StateController get() {
     return context.previousContext.asStateController()
 }
 
-public val StateController.root: StateController get() {
+public val StateController.rootController: StateController get() {
     return context.rootContext.asStateController()
 }
